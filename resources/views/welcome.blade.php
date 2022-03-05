436d2397312c883c081c0f5b9948476b3e992cb8
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-03-05 11:20:22 +0100

    Show passed variable to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,12 @@
                         </g>
                     </svg>
                 </div>
-
+                <h2>Todos:</h2>
+                <ul>
+                    @foreach ($todos as $todo)
+                        <li>{{ $todo->title }}</li>
+                    @endforeach
+                </ul>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
